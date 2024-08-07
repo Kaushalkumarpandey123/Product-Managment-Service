abcb7a9e05968860167733378f47e5cb4df422a3
Kaushal Pandey <kaushalpandey0039@gmail.com>
2024-08-07 23:47:04 +0530

    Added the project Architect and Product Service

---
+++ ProductService/ProductService/src/main/java/com/product/ProductService/ProductServiceApplication.java
@@ -0,0 +1,16 @@
+package com.product.ProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+
+		System.out.print("Product Service Started..");
+
+	}
+
+}
